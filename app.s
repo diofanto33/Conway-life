0be457ca36add6615ed9a890d34f4fd767d424d2
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-06-14 18:59:50 -0300

    [UPD] Start the game of life

--- app.s
+++ app.s
@@ -1,57 +1,62 @@
-	.equ SCREEN_WIDTH,   640
-	.equ SCREEN_HEIGH,   480
-	.equ BITS_PER_PIXEL, 32
+	
+.include "data.s"
+.include "utils.s"
+.include "game_life.s"
+.include "seed.s"
+.globl main
+   
+.equ SCREEN_PIXELS_div_2_menos_1, SCREEN_PIXELS/2 - 1
+/* Last index considering the elements as dword */ 
+screen_pixels_div_2_menos_1: .dword SCREEN_PIXELS_div_2_menos_1
 
-	.equ GPIO_BASE,    0x3f200000
-	.equ GPIO_GPFSEL0, 0x00
-	.equ GPIO_GPLEV0,  0x34
-
-	.globl main
+/*
+ * @brief: Copy everything from the secondary buffer to the frame buffer.
+ *  Executed once per frame.
+ *  The frame buffer address must be provided in dir_frameBuffer 
+ * 
+ * @param: x0 = Secondary buffer address 
+ * @return: None
+ * @saveStack: None
+ * @modifies: x9, x10, x11
+ */
+update_FB:
+    ldr x0, =bufferSecundario        
+    ldr x9, dir_frameBuffer
+    ldr x10, screen_pixels_div_2_menos_1
+loop_update_FB:
+    cmp x10, #0
+    b.lt end_loop_update_FB
+    ldr x11, [x0, x10, lsl #3]
+    str x11, [x9, x10, lsl #3]
+    sub x10, x10, #1
+    b loop_update_FB
+end_loop_update_FB:
+    br lr
 
 main:
-	// x0 contiene la direccion base del framebuffer
-	mov x20, x0 // Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
+    /* save the address of the frame-buffer in x20 */ 
+    mov x20, x0
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+    /* Load the address of the frame buffer into register x1 */  
+    adr x1, dir_frameBuffer
+    /* store the memory address of the frame-buffer in dir_frameBuffer */
+    str x0, [x1]
+    
+    /* draw the screen of black color */ 
+    bl draw_bg_fb2
+    
+    /* direct the seed */
+    mov x3, #40
+    mov x4, #40
+    /* draw the seed */ 
+    bl seed_glider
 
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4    // Siguiente pixel
-	sub x1,x1,1    // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1    // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+    /* update the frame-buffer */ 
+    bl update_FB
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
-
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
-
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
-
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10 (notar 0b... es binario)
-	// al inmediato se lo refiere como "máscara" en este caso:
-	// - Al hacer AND revela el estado del bit 2
-	// - Al hacer OR "setea" el bit 2 en 1
-	// - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
-	and w11, w10, 0b00000010
-
-	// si w11 es 0 entonces el GPIO 1 estaba liberado
-	// de lo contrario será distinto de 0, (en este caso particular 2)
-	// significando que el GPIO 1 fue presionado
-
-	//---------------------------------------------------------------
-	// Infinite Loop
-
-InfLoop:
-	b InfLoop
+/* start the game of life */ 
+game_loop:   
+    bl game_life
+    bl do_delay 
+    bl update_FB
+    b game_loop
